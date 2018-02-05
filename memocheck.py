fdebc73e34f827813f9d16e5554b2906cc74e3ad
akarinS <akaringithub0655@gmail.com>
2018-02-06 00:59:22 +0900

    Python3にして、koto/src/に置いて使うように変更。多分Macでも使える。

--- memocheck.py
+++ memocheck.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import commands
+import subprocess
 import json
 import datetime
 import sys
+import os
+import codecs
 
 ARGS = sys.argv
 ZADDRESS = ARGS[1]
-KOTOCLI = "~/koto/src/koto-cli "
+KOTOCLI = os.path.join(os.path.dirname(os.path.abspath(__file__)), "koto-cli ")
 DEFAULTMEMO = "f600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
-def get_received_data(ZADDRESS):
-  json_data = commands.getoutput(KOTOCLI + "z_listreceivedbyaddress " + ZADDRESS)
+def get_received_data(z_address):
+  json_data = subprocess.getoutput(KOTOCLI + "z_listreceivedbyaddress " + z_address)
   raw_data = json.loads(json_data)
 
   return raw_data
@@ -22,12 +24,12 @@ def get_transaction_data(raw_data):
   unsorted_data = []
 
   for a_data in raw_data:
-    transaction_json_data = commands.getoutput(KOTOCLI + "gettransaction " + a_data["txid"])
+    transaction_json_data = subprocess.getoutput(KOTOCLI + "gettransaction " + a_data["txid"])
     transaction_data = json.loads(transaction_json_data)
     
     if a_data["memo"] != DEFAULTMEMO:
       try:
-        memo = unicode(a_data["memo"].decode("hex_codec"), "UTF-8")
+        memo = codecs.decode(a_data["memo"], "hex_codec").decode("utf-8")
       except UnicodeDecodeError:
         memo = "---DecodeError---"
     else:
@@ -63,15 +65,19 @@ def get_sorted_data(unsorted_data):
 
 
 def print_each_data(sorted_data):
-  print("\n\n")
+  print("\n\n", end="")
 
   for a_data in sorted_data:
-    if a_data["memo"] != "---Empty---":
+#    if a_data["memo"] != "---Empty---":
       timestamp = datetime.datetime.fromtimestamp(a_data["time"])
       
       print(str(timestamp) + "  " + "{0:0.8f}".format(a_data["amount"]) + "KOTO  "+ a_data["txid"])
-      print(a_data["memo"])
-      print("\n")
+      print(a_data["memo"], end="\n\n")
 
-print_each_data(get_sorted_data(get_transaction_data(get_received_data(ZADDRESS))))
+def get_final_data(z_address):
+  return get_sorted_data(get_transaction_data(get_received_data(ZADDRESS)))
+
+
+if __name__ == '__main__':
+  print_each_data(get_final_data(ZADDRESS))
 
